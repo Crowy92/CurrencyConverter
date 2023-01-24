2738ca4bb0289a98682aab39248433dce3640639
Crowy92 <tom.crowther1992@gmail.com>
2023-01-24 08:49:28 +0000

    attempting a more complex flag selector

--- src/components/CurrencyRow.js
+++ src/components/CurrencyRow.js
@@ -10,6 +10,7 @@ export default function CurrencyRow(props) {
     onChangeCurrency
   } = props
   const [countries, setCountries] = useState()
+  const [hidden, setHidden] = useState(true)
   // const [countryCode, setCountryCode] = useState(selectedCurrency.slice(0,2))
 
   useEffect(() => {
@@ -20,24 +21,48 @@ export default function CurrencyRow(props) {
         })
     }, [])
 
+    function hideToggle () {
+      setHidden(prev => !prev)
+    }
+
   return (
     <div>
       {selectedCurrency && <img src={`https://flagcdn.com/60x45/${selectedCurrency.slice(0,2).toLowerCase()}.png`} width="16"
 height="12" alt={selectedCurrency.slice(0,2).toLowerCase()}></img>}
-        <select 
+        {/* <select 
             value={selectedCurrency} 
             onChange={onChangeCurrency}
             id='country_selector'
             className="selectpicker">
             {currencyOptions.map(option => {
-              // countryCode = option.slice(0, 2)
               return (<option 
                   key={option} 
                   value={option}>
                   {option}/{countries[option]}
               </option>)})
             }
-        </select>
+        </select> */}
+        {selectedCurrency && 
+        <div className="selector">
+          <div value={selectedCurrency} 
+            onChange={onChangeCurrency}
+            id='selectField' onClick={hideToggle}>
+            <img src={`https://flagcdn.com/60x45/${selectedCurrency.slice(0,2).toLowerCase()}.png`} alt={selectedCurrency}></img>
+            <p>{selectedCurrency}/{countries[selectedCurrency]}</p>
+            <ul id="list" hidden={hidden}>
+              {currencyOptions.map(curr => {
+                // countryCode = option.slice(0, 2)
+                return (<li 
+                    key={curr} 
+                    value={curr}>
+                    <img src={`https://flagcdn.com/60x45/${curr.slice(0,2).toLowerCase()}.png`} alt={curr}></img>
+                    <p>{curr}/{countries[curr]}</p>
+                </li>)})
+              }
+            </ul>
+          </div> 
+        </div>
+        }
     </div>
   )
 }
